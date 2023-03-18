c924659d89702bb3a6ed5f6b0f820c2371ad6ba6
pinterpol <pablo71042001@gmail.com>
2023-03-18 19:51:14 -0300

    Revert to 1eff3f87a6d9769d70587c16db872c6f00c8e239

--- omegle.js
+++ omegle.js
@@ -1,11 +1,11 @@
 //CODE MADE WITH LOVE BY @guaxinin and @al_interpol
-const { firefox } = require('playwright');
+const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await firefox.launch({ headless: false });
+  const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
-
+  
   await page.goto('https://www.omegle.com/');
   await page.waitForSelector('#textbtn');
 
@@ -13,15 +13,15 @@ const { firefox } = require('playwright');
   await page.click('#textbtn');
 
   //Check the first checkbox
-  const firstCheckBox = await page.$('label strong');
-  if (firstCheckBox) {
-    await firstCheckBox.click();
+  const firstCheck = await page.$('label strong');
+  if (firstCheck) {
+    await firstCheck.click();
   }
-
+  
   //Check the second checkbox
-  const ndCheckBox = await page.$("//label[contains(., 'By checking the box you acknowledge that you have reviewed and agree to be bound by ')]");
-  if (ndCheckBox) {
-    await ndCheckBox.click();
+  const ndCheck = await page.$("//label[contains(., 'By checking the box you acknowledge that you have reviewed and agree to be bound by ')]");
+  if (ndCheck) {
+    await ndCheck.click();
   }
 
   //Delay to see if the checkbox is checked
@@ -30,14 +30,18 @@ const { firefox } = require('playwright');
   //Click on confirm button
   await page.click('input[type="button"]');
 
+  //Verify if cinection exists
+  const connected = await page.$("//p[contains(., ' from the menu in the corner to disable.)')]");
+  if (connected) {
+    console.log('u r connected');
+  }
+
   //Verify if textarea exist and start loop
   while (true) {
     const textArea = await page.$('textarea.chatmsg');
-
     if (textArea) {
-      const enabledText = await textArea.isEnabled();
-      
-      if (enabledText) {
+      const isEnabled = await textArea.isEnabled();
+      if (isEnabled) {
         // Type into the textarea and send the message
         await textArea.type('Your message');
         await textArea.press('Enter');
@@ -46,18 +50,15 @@ const { firefox } = require('playwright');
         // Press the Esc key two times to go out of the chat
         await page.keyboard.press('Escape');
         await page.keyboard.press('Escape');
-        console.log('Starting a new chat');
-      
+        console.log('Starting a new chat')
       } else {
         //if textarea is not enabled, start another chat
         console.log('Textarea element is disabled. Starting a new chat...');
         await page.keyboard.press('Escape');
       }
-    
     } else {
       console.log('Bonk! Textarea element not found.');
     }
-    
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(10000);
   }
 })();
