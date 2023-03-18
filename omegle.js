3cce3400c5c1ce71adaa8e9d3b874df0a5347fd5
pinterpol <pablo71042001@gmail.com>
2023-03-18 20:06:27 -0300

    Solving some problems and adding Telegram Bot support

--- omegle.js
+++ omegle.js
@@ -1,13 +1,20 @@
 //CODE MADE WITH LOVE BY @guaxinin and @al_interpol
 const { chromium } = require('playwright');
+const TelegramBot = require('node-telegram-bot-api');
 
 (async () => {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
+  const bot = new TelegramBot('YOUR_BOT_TOKEN');
+  const myChatId = 'YOUR_CHAT_ID';
   
   await page.goto('https://www.omegle.com/');
+  console.log('Initiating process...');
+  bot.sendAnimation(myChatId, 'https://media4.giphy.com/media/6pUjuQQX9kEfSe604w/giphy.gif?cid=ecf05e47aokiybnwwcb9r9fvsqg58y0xwaztfennjib0d42e&rid=giphy.gif&ct=g', {caption: 'Iniciando rotina...'});
+
   await page.waitForSelector('#textbtn');
+  await page.waitForTimeout(1500);
 
   // Click the text-only chat button
   await page.click('#textbtn');
@@ -15,12 +22,14 @@ const { chromium } = require('playwright');
   //Check the first checkbox
   const firstCheck = await page.$('label strong');
   if (firstCheck) {
+    await page.waitForTimeout(500);
     await firstCheck.click();
   }
   
   //Check the second checkbox
   const ndCheck = await page.$("//label[contains(., 'By checking the box you acknowledge that you have reviewed and agree to be bound by ')]");
   if (ndCheck) {
+    await page.waitForTimeout(1000);
     await ndCheck.click();
   }
 
@@ -30,12 +39,6 @@ const { chromium } = require('playwright');
   //Click on confirm button
   await page.click('input[type="button"]');
 
-  //Verify if cinection exists
-  const connected = await page.$("//p[contains(., ' from the menu in the corner to disable.)')]");
-  if (connected) {
-    console.log('u r connected');
-  }
-
   //Verify if textarea exist and start loop
   while (true) {
     const textArea = await page.$('textarea.chatmsg');
@@ -43,7 +46,8 @@ const { chromium } = require('playwright');
       const isEnabled = await textArea.isEnabled();
       if (isEnabled) {
         // Type into the textarea and send the message
-        await textArea.type('Your message');
+        await textArea.type('Your message', { delay: 100 });
+        await page.waitForTimeout(700);
         await textArea.press('Enter');
         console.log('Sent message');
         await page.waitForTimeout(2000);
@@ -58,7 +62,9 @@ const { chromium } = require('playwright');
       }
     } else {
       console.log('Bonk! Textarea element not found.');
+      bot.sendMessage(myChatId, 'O bot está sendo desligado. 💤');
+      await browser.close();
     }
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(1000);
   }
 })();
